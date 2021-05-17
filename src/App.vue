e34caad2b3d4264adfce0de2b35e3e1f13f7c88e
Tanyiqu <1953649096@qq.com>
2021-05-17 23:35:12 +0800

    调整主页布局

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
 <template>
   <div id="app">
-    <div id="header">header</div>
+    <div id="header">
+      <div class="logo">
+        <p class="desc">TanTools - 又是一个便捷的在线工具网站</p>
+      </div>
+    </div>
     <div id="content">
       <router-view />
     </div>
-    <div id="footer">footer</div>
+    <div id="footer" @click="toTanyiqu">Code by tanyiqu</div>
   </div>
 </template>
 
+<script>
+export default {
+  methods: {
+    toTanyiqu() {
+      window.open('https://github.com/tanyiqu', '_blank');
+    }
+  },
+}
+</script>
+
 <style lang="less">
 @header_height: 50px;
-@footer_height: 38px;
+@footer_height: 30px;
 
 #app {
   height: 100%;
@@ -23,6 +37,21 @@
     height: @header_height;
     background: #fff;
     box-shadow: 0 2px 4px -1px #cccccc;
+    display: flex;
+    align-items: center;
+
+    .logo {
+      display: flex;
+      align-items: center;
+
+      .img {
+      }
+
+      .desc {
+        position: absolute;
+        right: 20px;
+      }
+    }
   }
 
   #content {
@@ -37,6 +66,10 @@
     height: @footer_height;
     background: #fff;
     box-shadow: 0 -1px 2px #dddddd;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
   }
 }
 </style>
